c0c83d617cb62fdf572e705c52e1cbce5322ff38
daynawhite <dayna26.2@gmail.com>
2023-12-06 21:01:05 -0600

    3 tests pass

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,20 +1,52 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+    if (num % 2 == 0) {
+      return true
+    } else {return false}
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
-}
+  return arr.reduce((acc,curr) => acc + curr)
+  };
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
-}
+  if (arr.reduce((acc,curr) => acc + curr) === sum) {
+    return true
+  } else {return false}
+  }
 
 module.exports = {
   isEven,
   sum,
   comboSum
+}
+
+// describe('Strings module', () => {
+
+//   test('Split a string on the given delimeter', () => {
+//     const split = strings.split('a-new-string', '-')
+//     expect(split.length).toEqual(3)
+//     expect(split[0]).toBe('a')
+//     expect(split[1]).toBe('new')
+//     expect(split[2]).toBe('string')
+//   })
+
+const split = (string, delimeter) => {
+  return split(string, delimeter)
+}
+
+const pairs = () => {
+
+}
+
+const reversed = () => {
+
+}
+
+module.exports = {
+  split,
+  pairs,
+  reversed
 }
